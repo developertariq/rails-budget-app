147cf12f2dbe10a38f7032da3923e9bdb57ed5fd
developertariq <dev.tariqul.islam@gmail.com>
2023-04-28 19:48:51 +0600

    Implement devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'splash#splashpage'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+
+  resources :categories do 
+    resources :expenses 
+  end
 end
