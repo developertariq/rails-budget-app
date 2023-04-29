b92578715378439f006f067f7d6336233f0bb5c7
developertariq <dev.tariqul.islam@gmail.com>
2023-04-29 16:00:37 +0600

    Fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # Rails.application.credentials.secret_key_base
-  config.require_master_key = true
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -19,7 +19,7 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
