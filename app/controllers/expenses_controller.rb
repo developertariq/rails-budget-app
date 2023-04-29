df149786d91f759d736eeae1fa0ac90291fa7fc4
developertariq <dev.tariqul.islam@gmail.com>
2023-04-29 23:34:38 +0600

    Fix linters

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,20 +1,25 @@
 class ExpensesController < ApplicationController
   def index
     @category = Category.find(params[:category_id])
-    @expenses = @category.expenses.all
+    @category_expenses = @category.category_expenses
+    @expenses = Expense.joins(:category_expenses, :categories)
+      .where('expenses.id = category_expenses.expense_id')
+      .where('categories.id = category_expenses.category_id')
+      .where('categories.id = ?', params[:category_id]).order('expenses.created_at DESC')
   end
 
   def new
-    @category = Category.find(params[:category_id])
-    @expense = @category.expenses.new
+    @expense = Expense.new
+    @categories = current_user.categories
   end
 
   def create
-    @category = Category.find(params[:category_id])
-    @expense = @category.expenses.new(expense_params)
+    @expense = Expense.new(name: params[:name], amount: params[:amount], author_id: params[:author_id])
 
     if @expense.save
-      redirect_to category_expenses_path(@category), notice: 'Transaction created successfully.'
+      category_expense = CategoryExpense.new(category_id: params[:category], expense_id: @expense.id)
+      category_expense.save
+      redirect_to category_expenses_path(params[:category]), notice: 'Transaction created successfully.'
     else
       render :new
     end
@@ -23,6 +28,6 @@ class ExpensesController < ApplicationController
   private
 
   def expense_params
-    params.require(:expense).permit(:name, :amount, :category_id, :author_id)
+    params.require(:expense).permit(:name, :amount, :author_id)
   end
 end
